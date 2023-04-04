2edf2cf7bf8b64b7cedb5564eb907cd4c52ffe19
Syrus Akbary <me@syrusakbary.com>
2023-04-03 19:49:09 -0700

    Improved index caching

--- app/app.php
+++ app/app.php
@@ -150,13 +150,13 @@ if (SERVE_STATIC && is_file($requestedAbsoluteFile)) {
         header("Last-Modified: " . gmdate("D, d M Y H:i:s", $last_modified_time) . " GMT");
         header("Etag: $etag");
 
-        // if (
-        //     @strtotime($_SERVER['HTTP_IF_MODIFIED_SINCE']) == $last_modified_time ||
-        //     trim($_SERVER['HTTP_IF_NONE_MATCH']) == $etag
-        // ) {
-        //     header("HTTP/1.1 304 Not Modified");
-        //     exit;
-        // }
+        if (
+            @strtotime($_SERVER['HTTP_IF_MODIFIED_SINCE']) == $last_modified_time ||
+            trim($_SERVER['HTTP_IF_NONE_MATCH']) == $etag
+        ) {
+            header("HTTP/1.1 304 Not Modified");
+            exit;
+        }
 
         $fh = fopen($requestedAbsoluteFile, 'r');
         fpassthru($fh);
@@ -181,6 +181,8 @@ if (SERVE_STATIC && is_file($requestedAbsoluteFile)) {
             ob_end_flush();
         }
     } else {
-        require __DIR__ . '/index.php';
+        if (!preg_match('/\.(css|txt|js|png|jpg|jpeg|json|htm|html|csv)$/', $requestedAbsoluteFile)) {
+            require __DIR__ . '/index.php';
+        }
     }
 }
