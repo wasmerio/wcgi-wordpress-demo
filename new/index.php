8ef30a5ef7bff46d9cf9fe736401a32d72266098
Syrus Akbary <me@syrusakbary.com>
2023-04-03 18:45:54 -0700

    Improved minimal example

--- new/index.php
+++ new/index.php
@@ -1,40 +1,14 @@
 <?php
 
-/// Use this if you want to use with local php:
-/// cd new && php -S localhost:8000
-// define("DB_PATH", dirname(__DIR__)."/db/.ht.sqlite");
 
-/// Use this if you want to use with Wasmer:
-/// cd new && wasmer-dev run-unstable .. --mapdir=/db:../db
-define("DB_PATH", "/db/.ht.sqlite");
-
-echo "DB at: ". DB_PATH;
-
-class MyDB extends SQLite3 {
-    function __construct() {
-       $this->open(DB_PATH);
-    }
- }
- 
- $db = new MyDB();
- if(!$db) {
-    echo $db->lastErrorMsg();
- } else {
-    echo "Opened database successfully\n";
- }
-
- $sql =<<<EOF
-    SELECT * from wp_posts;
-EOF;
-
- $ret = $db->query($sql);
- if (!$ret) {
-    echo $db->lastErrorMsg();
- }
- else {
- while($row = $ret->fetchArray(SQLITE3_ASSOC) ) {
-    var_dump($row);
- }
- echo "Operation done successfully\n";
+register_shutdown_function('shutdown');
+function shutdown(){
+    error_log("debug: shutting down");
 }
- $db->close();
+
+echo "Hello World!";
+error_log("debug: before exit");
+exit(0);
+// throw new Exception('Division by zero.');
+error_log("debug: after exit");
+echo "Goodbye World!";
