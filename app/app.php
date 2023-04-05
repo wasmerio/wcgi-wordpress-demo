ef1550ba3cce4fb6882a738d8409aacbc09784f0
Syrus Akbary <me@syrusakbary.com>
2023-04-04 21:18:54 -0700

    Commented code

--- app/app.php
+++ app/app.php
@@ -150,13 +150,14 @@ if (SERVE_STATIC && is_file($requestedAbsoluteFile)) {
         header("Last-Modified: " . gmdate("D, d M Y H:i:s", $last_modified_time) . " GMT");
         header("Etag: $etag");
 
-        if (
-            @strtotime($_SERVER['HTTP_IF_MODIFIED_SINCE']) == $last_modified_time ||
-            trim($_SERVER['HTTP_IF_NONE_MATCH']) == $etag
-        ) {
-            header("HTTP/1.1 304 Not Modified");
-            exit;
-        }
+        // if (
+        //     @strtotime($_SERVER['HTTP_IF_MODIFIED_SINCE']) == $last_modified_time ||
+        //     trim($_SERVER['HTTP_IF_NONE_MATCH']) == $etag
+        // ) {
+        //     header("HTTP/1.1 304 Not Modified");
+        //     echo " ";
+        //     echo " "; exit(0);
+        // }
 
         $fh = fopen($requestedAbsoluteFile, 'r');
         fpassthru($fh);
